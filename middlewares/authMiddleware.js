c26f4ffb75a9ebffd458b158d8252d204463b7e1
NameLess0974 <ursancristiancm@gmail.com>
2024-12-21 17:14:11 +0100

    Middle

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,14 @@
+// middlewares/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
 exports.authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader?.split(' ')[1]; // Utilisation d'opérateur optionnel pour plus de concision
-
-  if (!token) {
-    return res.status(401).json({ message: 'Token d\'authentification manquant' });
-  }
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401);
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: 'Token invalide ou expiré' });
-    }
+    if (err) return res.sendStatus(403);
+    console.log('Decoded JWT:', user); // Ajoutez cette ligne pour déboguer
     req.user = user;
     next();
   });
--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,10 +1,9 @@
 // middlewares/roleMiddleware.js
 exports.authorizeRoles = (...roles) => {
-    return (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
-        return res.status(403).json({ message: 'Accès refusé : rôle non autorisé' });
-      }
-      next();
-    };
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Accès refusé : rôle non autorisé' });
+    }
+    next();
   };
-  
+};
