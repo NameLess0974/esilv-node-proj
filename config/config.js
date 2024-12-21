aa2640934d1bf256ef7e8b1486a16288b21e7dbf
NameLess0974 <ursancristiancm@gmail.com>
2024-12-21 16:55:22 +0100

    MAJ

--- config/config.js
+++ config/config.js
@@ -1,27 +1,29 @@
+// config/config.js
 require('dotenv').config();
-
-const defaultConfig = {
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'your_postgres_password',
-  host: process.env.DB_HOST || '127.0.0.1',
-  dialect: 'postgres', // Modifier selon la base utilisée
-  logging: false, // Désactive le logging SQL
-};
 
 module.exports = {
   development: {
-    ...defaultConfig,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'your_postgres_password',
     database: process.env.DB_NAME || 'gestion_bibliotheque',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'postgres', // ou 'mysql' si vous utilisez MySQL
+    logging: false, // Désactiver le logging SQL
   },
   test: {
-    ...defaultConfig,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'your_postgres_password',
     database: process.env.DB_NAME || 'gestion_bibliotheque_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'postgres',
+    logging: false,
   },
   production: {
-    ...defaultConfig,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
   },
 };
